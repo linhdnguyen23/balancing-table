665d1da0d6fb24e46e7c27009e048a0e86d14cca
Linh Nguyen <linh.nguyen@omron.com>
2022-03-23 09:52:14 -0700

    get calibration points

--- TouchScreenCode/TouchScreenCode.ino
+++ TouchScreenCode/TouchScreenCode.ino
@@ -15,45 +15,40 @@ const byte sensePin = A0;
 int x, y, count;
 POINT *calibPts = new POINT[3];
 
-int setCalibrationMatrix( POINT * displayPtr,
-                          POINT * screenPtr,
-                          MATRIX * matrixPtr) {
+int setCalibrationMatrix(POINT * displayPtr, POINT * screenPtr, MATRIX * matrixPtr) {
 
-    int  retValue = OK ;
+  int  retValue = OK ;
 
 
 
-    matrixPtr->Divider = ((screenPtr[0].x - screenPtr[2].x) * (screenPtr[1].y - screenPtr[2].y)) -
-                         ((screenPtr[1].x - screenPtr[2].x) * (screenPtr[0].y - screenPtr[2].y)) ;
+  matrixPtr->Divider = ((screenPtr[0].x - screenPtr[2].x) * (screenPtr[1].y - screenPtr[2].y)) -
+                       ((screenPtr[1].x - screenPtr[2].x) * (screenPtr[0].y - screenPtr[2].y));
 
-    if ( matrixPtr->Divider == 0 )
-    {
-        retValue = NOT_OK ;
-    }
-    else
-    {
-        matrixPtr->An = ((displayPtr[0].x - displayPtr[2].x) * (screenPtr[1].y - screenPtr[2].y)) -
-                        ((displayPtr[1].x - displayPtr[2].x) * (screenPtr[0].y - screenPtr[2].y)) ;
+  if (matrixPtr->Divider == 0) {
+      retValue = NOT_OK ;
+  } else {
+    matrixPtr->An = ((displayPtr[0].x - displayPtr[2].x) * (screenPtr[1].y - screenPtr[2].y)) -
+                    ((displayPtr[1].x - displayPtr[2].x) * (screenPtr[0].y - screenPtr[2].y));
 
-        matrixPtr->Bn = ((screenPtr[0].x - screenPtr[2].x) * (displayPtr[1].x - displayPtr[2].x)) -
-                        ((displayPtr[0].x - displayPtr[2].x) * (screenPtr[1].x - screenPtr[2].x)) ;
+    matrixPtr->Bn = ((screenPtr[0].x - screenPtr[2].x) * (displayPtr[1].x - displayPtr[2].x)) -
+                    ((displayPtr[0].x - displayPtr[2].x) * (screenPtr[1].x - screenPtr[2].x));
 
-        matrixPtr->Cn = (screenPtr[2].x * displayPtr[1].x - screenPtr[1].x * displayPtr[2].x) * screenPtr[0].y +
-                        (screenPtr[0].x * displayPtr[2].x - screenPtr[2].x * displayPtr[0].x) * screenPtr[1].y +
-                        (screenPtr[1].x * displayPtr[0].x - screenPtr[0].x * displayPtr[1].x) * screenPtr[2].y ;
+    matrixPtr->Cn = (screenPtr[2].x * displayPtr[1].x - screenPtr[1].x * displayPtr[2].x) * screenPtr[0].y +
+                    (screenPtr[0].x * displayPtr[2].x - screenPtr[2].x * displayPtr[0].x) * screenPtr[1].y +
+                    (screenPtr[1].x * displayPtr[0].x - screenPtr[0].x * displayPtr[1].x) * screenPtr[2].y;
 
-        matrixPtr->Dn = ((displayPtr[0].y - displayPtr[2].y) * (screenPtr[1].y - screenPtr[2].y)) -
-                        ((displayPtr[1].y - displayPtr[2].y) * (screenPtr[0].y - screenPtr[2].y)) ;
+    matrixPtr->Dn = ((displayPtr[0].y - displayPtr[2].y) * (screenPtr[1].y - screenPtr[2].y)) -
+                    ((displayPtr[1].y - displayPtr[2].y) * (screenPtr[0].y - screenPtr[2].y));
 
-        matrixPtr->En = ((screenPtr[0].x - screenPtr[2].x) * (displayPtr[1].y - displayPtr[2].y)) -
-                        ((displayPtr[0].y - displayPtr[2].y) * (screenPtr[1].x - screenPtr[2].x)) ;
+    matrixPtr->En = ((screenPtr[0].x - screenPtr[2].x) * (displayPtr[1].y - displayPtr[2].y)) -
+                    ((displayPtr[0].y - displayPtr[2].y) * (screenPtr[1].x - screenPtr[2].x));
 
-        matrixPtr->Fn = (screenPtr[2].x * displayPtr[1].y - screenPtr[1].x * displayPtr[2].y) * screenPtr[0].y +
-                        (screenPtr[0].x * displayPtr[2].y - screenPtr[2].x * displayPtr[0].y) * screenPtr[1].y +
-                        (screenPtr[1].x * displayPtr[0].y - screenPtr[0].x * displayPtr[1].y) * screenPtr[2].y ;
-    }
+    matrixPtr->Fn = (screenPtr[2].x * displayPtr[1].y - screenPtr[1].x * displayPtr[2].y) * screenPtr[0].y +
+                    (screenPtr[0].x * displayPtr[2].y - screenPtr[2].x * displayPtr[0].y) * screenPtr[1].y +
+                    (screenPtr[1].x * displayPtr[0].y - screenPtr[0].x * displayPtr[1].y) * screenPtr[2].y;
+  }
 
-    return( retValue ) ;
+  return retValue;
 
 } /* end of setCalibrationMatrix() */
 
@@ -66,24 +61,24 @@ int setCalibrationMatrix( POINT * displayPtr,
  *  Description: Given a valid set of calibration factors and a point
  *                value reported by the touch screen, this function
  *                calculates and returns the true (or closest to true)
- *                display point below the spot where the touch screen 
+ *                display point below the spot where the touch screen
  *                was touched.
- * 
  *
- * 
+ *
+ *
  *  Argument(s): displayPtr (output) - Pointer to the calculated
  *                                      (true) display point.
  *               screenPtr (input) - Pointer to the reported touch
  *                                    screen point.
  *               matrixPtr (input) - Pointer to calibration factors
  *                                    matrix previously calculated
- *                                    from a call to 
+ *                                    from a call to
  *                                    setCalibrationMatrix()
- * 
  *
- *  The function simply solves for Xd and Yd by implementing the 
- *   computations required by the translation matrix.  
- * 
+ *
+ *  The function simply solves for Xd and Yd by implementing the
+ *   computations required by the translation matrix.
+ *
  *                                              /-     -\
  *              /-    -\     /-            -\   |       |
  *              |      |     |              |   |   Xs  |
@@ -93,11 +88,11 @@ int setCalibrationMatrix( POINT * displayPtr,
  *              |      |     |              |   |   1   |
  *              \-    -/     \-            -/   |       |
  *                                              \-     -/
- * 
- *  It must be kept brief to avoid consuming CPU cycles.
- * 
  *
- *       Return: OK - the display point was correctly calculated 
+ *  It must be kept brief to avoid consuming CPU cycles.
+ *
+ *
+ *       Return: OK - the display point was correctly calculated
  *                     and its value is in the output argument.
  *               NOT_OK - an error was detected and the function
  *                         failed to return a valid point.
@@ -107,101 +102,115 @@ int setCalibrationMatrix( POINT * displayPtr,
  *                 NOTE!    NOTE!    NOTE!
  *
  *  setCalibrationMatrix() and getDisplayPoint() will do fine
- *  for you as they are, provided that your digitizer         
+ *  for you as they are, provided that your digitizer
  *  resolution does not exceed 10 bits (1024 values).  Higher
  *  resolutions may cause the integer operations to overflow
- *  and return incorrect values.  If you wish to use these   
- *  functions with digitizer resolutions of 12 bits (4096    
- *  values) you will either have to a) use 64-bit signed     
- *  integer variables and math, or b) judiciously modify the 
- *  operations to scale results by a factor of 2 or even 4.  
+ *  and return incorrect values.  If you wish to use these
+ *  functions with digitizer resolutions of 12 bits (4096
+ *  values) you will either have to a) use 64-bit signed
+ *  integer variables and math, or b) judiciously modify the
+ *  operations to scale results by a factor of 2 or even 4.
  *
  *
  */
-int getDisplayPoint( POINT * displayPtr,
-                     POINT * screenPtr,
-                     MATRIX * matrixPtr ) {
-    int  retValue = OK ;
+int getDisplayPoint(POINT * displayPtr, POINT * screenPtr, MATRIX * matrixPtr) {
+  int  retValue = OK ;
 
 
-    if ( matrixPtr->Divider != 0 )
-    {
+  if (matrixPtr->Divider != 0) {
 
-        /* Operation order is important since we are doing integer */
-        /*  math. Make sure you add all terms together before      */
-        /*  dividing, so that the remainder is not rounded off     */
-        /*  prematurely.                                           */
+    /* Operation order is important since we are doing integer */
+    /*  math. Make sure you add all terms together before      */
+    /*  dividing, so that the remainder is not rounded off     */
+    /*  prematurely.                                           */
 
-        displayPtr->x = ( (matrixPtr->An * screenPtr->x) +
-                          (matrixPtr->Bn * screenPtr->y) +
-                          matrixPtr->Cn
-                        ) / matrixPtr->Divider ;
+    displayPtr->x = ((matrixPtr->An * screenPtr->x) +
+                      (matrixPtr->Bn * screenPtr->y) +
+                      matrixPtr->Cn
+                    ) / matrixPtr->Divider;
 
-        displayPtr->y = ( (matrixPtr->Dn * screenPtr->x) +
-                          (matrixPtr->En * screenPtr->y) +
-                          matrixPtr->Fn
-                        ) / matrixPtr->Divider ;
-    }
-    else
-    {
-        retValue = NOT_OK ;
-    }
+    displayPtr->y = ((matrixPtr->Dn * screenPtr->x) +
+                      (matrixPtr->En * screenPtr->y) +
+                      matrixPtr->Fn
+                    ) / matrixPtr->Divider;
+  } else {
+    retValue = NOT_OK ;
+  }
 
-    return( retValue ) ;
-
+  return retValue;
 } /* end of getDisplayPoint() */
 
-void waitForPoint(POINT* pts, int idx) {
-  
+void populatePointArr(POINT* pts, int idx) {
+  setReadXMode();
+  pts[idx].x = analogRead(sensePin);
+  setReadYMode();
+  pts[idx].y = analogRead(sensePin);
 }
 
 void setup() {
-	// Configure corner pins as outputs
-	for(int i=0; i<4; i++){
-		pinMode(cornerPins[i], OUTPUT);
-	}
-	// Start serial connection to monitor output
-	Serial.begin(9600);
-
-	Serial.print("Enter 1st point:");
-	waitForPoint(calibPts, 0);
-	Serial.print("Enter 2nd point:");
-	waitForPoint(calibPts, 1);
-	Serial.print("Enter 3rd point:");
-	waitForPoint(calibPts, 2);
-  
+  // Configure corner pins as outputs
+  for(int i=0; i<4; i++){
+    pinMode(cornerPins[i], OUTPUT);
+  }
+  // Start serial connection to monitor output
+  Serial.begin(9600);
+  Serial.print("Begin calibration.");
+  char buffer[50];
+  for (int i = 0; i < 3; i++) {
+    setStandbyMode();
+    sprintf(buffer, "Please press and hold the calibration point #",i + 1);
+    Serial.print (buffer);
+    boolean touchDetected = false;
+    while (!touchDetected) {
+      touchDetected = analogRead(sensePin);
+    }
+    populatePointArr(calibPts, i);
+  }
 }
 
+void setStandbyMode() {
+  digitalWrite(cornerPins[0], HIGH);
+  digitalWrite(cornerPins[1], HIGH);
+  digitalWrite(cornerPins[2], LOW);
+  digitalWrite(cornerPins[3], HIGH);
+  delay(1);
+}
 
+void setReadXMode() {
+  // Set the left-hand corner pins LOW and the right-hand corner pins HIGH
+  digitalWrite(cornerPins[0], HIGH);
+  digitalWrite(cornerPins[1], HIGH);
+  digitalWrite(cornerPins[2], LOW);
+  digitalWrite(cornerPins[3], LOW);
+  // Read the sensor value in the x gradient
+  delay(1);
+}
 
+void setReadYMode() {
+  // Set the bottom corner pins LOW and the top corner pins HIGH
+  digitalWrite(cornerPins[0], HIGH);
+  digitalWrite(cornerPins[1], LOW);
+  digitalWrite(cornerPins[2], HIGH);
+  digitalWrite(cornerPins[3], LOW);
+  // Read the sensor value in the y gradient
+  delay(1);
+}
 void loop() {
-	// Set the left-hand corner pins LOW and the right-hand corner pins HIGH
-	digitalWrite(cornerPins[0], HIGH);
-	digitalWrite(cornerPins[1], HIGH);
-	digitalWrite(cornerPins[2], LOW);
-	digitalWrite(cornerPins[3], LOW);
-	// Read the sensor value in the x gradient
-	delay(1);
+  setReadXMode();
 	x = analogRead(sensePin);
-	// resolution? 
+	// resolution?
 
-	// Set the bottom corner pins LOW and the top corner pins HIGH
-	digitalWrite(cornerPins[0], HIGH);
-	digitalWrite(cornerPins[1], LOW);
-	digitalWrite(cornerPins[2], HIGH);
-	digitalWrite(cornerPins[3], LOW);
-	// Read the sensor value in the y gradient
-	delay(1);
-	y = analogRead(sensePin);
-
-	count=millis();
-
-	// Display the co-ordinate value obtained
-	Serial.print("X:");
-	Serial.print(x);  
-	Serial.print(",");
-	Serial.print("Y:");
-	Serial.println(y);
-	Serial.print("Time");
-	Serial.print(count);
+  setReadYMode();
+  y = analogRead(sensePin);
+  
+  count=millis();
+  
+  // Display the co-ordinate value obtained
+  Serial.print("X:");
+  Serial.print(x);
+  Serial.print(",");
+  Serial.print("Y:");
+  Serial.println(y);
+  Serial.print("Time");
+  Serial.print(count);
 }
--- TouchScreenCode/calibrate.h
+++ TouchScreenCode/calibrate.h
@@ -1,14 +1,14 @@
 #ifndef		_CALIBRATE_C_
-	#define		EXTERN         extern
+  #define		EXTERN         extern
 #else
-	#define		EXTERN
+  #define		EXTERN
 #endif
 
 
 
 #ifndef		OK
-	#define		OK		        0
-	#define		NOT_OK		   -1
+  #define		OK		        0
+  #define		NOT_OK		   -1
 #endif
 
 
@@ -25,23 +25,23 @@
 /****************************************************/
 
 struct POINT { 
-   INT32    x;
-   INT32    y;
+  INT32    x;
+  INT32    y;
 };
 
 
 #define  MATRIX_Type  INT32
 
 struct MATRIX {
-/* This arrangement of values facilitates 
-*  calculations within getDisplayPoint() 
-*/
-   MATRIX_Type    An;     /* A = An/Divider */
-   MATRIX_Type    Bn;     /* B = Bn/Divider */
-   MATRIX_Type    Cn;     /* C = Cn/Divider */
-   MATRIX_Type    Dn;     /* D = Dn/Divider */
-   MATRIX_Type    En;     /* E = En/Divider */
-   MATRIX_Type    Fn;     /* F = Fn/Divider */
-   MATRIX_Type    Divider ;
+  /* This arrangement of values facilitates 
+  *  calculations within getDisplayPoint() 
+  */
+  MATRIX_Type    An;     /* A = An/Divider */
+  MATRIX_Type    Bn;     /* B = Bn/Divider */
+  MATRIX_Type    Cn;     /* C = Cn/Divider */
+  MATRIX_Type    Dn;     /* D = Dn/Divider */
+  MATRIX_Type    En;     /* E = En/Divider */
+  MATRIX_Type    Fn;     /* F = Fn/Divider */
+  MATRIX_Type    Divider ;
 };
 
